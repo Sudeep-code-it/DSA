3b19cf422108e8bb0d63eaa8d5567a705bc28d34
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-05-26 11:30:27 +0530

    Create CominationSum.cpp

---
+++ DSA/Problems/CominationSum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+     void allposs(vector<int>&c,int t,set<vector<int>>&ans,vector<int>cv)
+     {
+         if(t==0)
+         {
+             if(cv.size()!=0)
+             {
+              sort(cv.begin(),cv.end());
+              ans.insert(cv);
+             }
+              return ;
+         }
+         if(t<0) return ;
+
+         for(int i=0;i<c.size();i++)
+         {
+            
+              cv.push_back(c[i]);
+              allposs(c,t-c[i],ans,cv);
+              cv.pop_back();
+             
+
+         }
+     }
+    
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        set<vector<int>>ans;
+         vector<int>v;
+        allposs(c,t,ans,v);
+
+        vector<vector<int>>ret;
+
+        for(auto it=ans.begin();it!=ans.end();it++)
+        ret.push_back(*it);
+
+        return ret;
+    }
+};
