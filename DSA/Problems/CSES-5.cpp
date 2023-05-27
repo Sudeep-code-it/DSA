6f49893b4e25be023f14b3d8390a93e7803a4be4
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-05-27 15:36:52 +0530

    CSES-5.cpp

---
+++ DSA/Problems/CSES-5.cpp
@@ -0,0 +1,26 @@
+
+// https://cses.fi/problemset/task/1070
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int>v(n);
+    if (n == 2 || n == 3)
+        cout << "NO SOLUTION";
+    else
+    {
+        for (int i = 2; i <= n; i += 2)
+            cout << i << " ";
+        for (int i = 1; i <= n; i += 2)
+            cout << i << " ";
+
+    }
+    return 0;
+
+}
