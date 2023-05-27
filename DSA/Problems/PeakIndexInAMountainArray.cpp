f05b204eb177609c78bca695e77da5c8290c200b
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-05-27 12:03:10 +0530

    PeakIndexInAMountainArray.cpp

---
+++ DSA/Problems/PeakIndexInAMountainArray.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0,e=arr.size()-1;
+
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
+            else if(arr[mid]>arr[mid+1]) e=mid-1;
+            else s=mid+1;
+
+        }
+
+        return s;
+    }
+};
