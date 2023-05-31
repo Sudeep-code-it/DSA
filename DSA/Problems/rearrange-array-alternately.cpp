f4d5221caee217e6b2f57e5448f9cba1ef1d0a84
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-05-31 20:38:39 +0530

    Update rearrange-array-alternately.cpp

--- DSA/Problems/rearrange-array-alternately.cpp
+++ DSA/Problems/rearrange-array-alternately.cpp
@@ -1,5 +1,5 @@
 // https://practice.geeksforgeeks.org/problems/-rearrange-array-alternately-1587115620/1
-
+/*  using extra space
 class Solution{
     public:
  
@@ -17,6 +17,35 @@ class Solution{
       
       for(int i=0;i<n;i++)
       arr[i]=v[i];
+    	 
+    }
+};
+*/
+
+// no extra space
+class Solution{
+    public:
+ 
+    void rearrange(long long *arr, int n) 
+    { 
+       int max_ind=n-1;
+       int min_ind=0;
+       int max=arr[n-1]+1;
+
+      for(int i=0;i<n;i++)
+      {
+          if(i%2==0)
+          {arr[i]=(arr[max_ind]%max)*max+arr[i];
+              max_ind--;
+          }
+          else
+          {arr[i]=(arr[min_ind]%max)*max+arr[i];
+              min_ind++;
+          }
+      }
+      
+      for(int i=0;i<n;i++)
+      arr[i]/=max;
     	 
     }
 };
