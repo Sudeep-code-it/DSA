64a0e6b076d299f3923975d8038f426e6762c686
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-06-12 22:25:56 +0530

    boundary-traversal-of-binary-tree.cpp

---
+++ DSA/Problems/boundary-traversal-of-binary-tree.cpp
@@ -0,0 +1,45 @@
+// https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1
+
+
+void left(Node *root)
+{
+    if(root->left==NULL && root->right==NULL) return;
+    ans.push_back(root->data);
+    if(root->left!=NULL)left(root->left);
+    if(root->left==NULL && root->right!=NULL)left(root->right);
+}
+void right(Node *root)
+{
+    if(root->left==NULL && root->right==NULL) return;
+    st.push(root->data);
+    if(root->right!=NULL)right(root->right);
+    if(root->left!=NULL && root->right==NULL)right(root->left);
+}
+void dfs(Node *root)
+{
+     if(root->left==NULL && root->right==NULL) 
+     {
+         ans.push_back(root->data);
+     return;}
+    if(root->left!=NULL)dfs(root->left);
+    if(root->right!=NULL)dfs(root->right);
+}
+    vector <int> boundary(Node *root)
+    {
+        
+        ans.push_back(root->data);
+        if(root->left!=NULL) left(root->left);
+        if(root->right!=NULL) right(root->right);
+        
+        if(root->left==NULL && root->right==NULL) return ans;
+        dfs(root);
+        
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+        
+    }
+};
