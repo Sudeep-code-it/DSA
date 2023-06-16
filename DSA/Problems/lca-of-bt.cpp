c613a04f714da24229d551fa9daa3a897fb69e2b
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-06-17 02:02:29 +0530

    lca-of-bt.cpp

---
+++ DSA/Problems/lca-of-bt.cpp
@@ -0,0 +1,29 @@
+// https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1
+
+
+class Solution
+{
+    public:
+     
+     
+     
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       if(root==NULL) return root;
+       
+       if(root->data==n1 || root->data==n2)
+       return root;
+       
+       
+       Node* l=lca(root->left,n1,n2);
+       Node* r=lca(root->right,n1,n2);
+       
+       
+       if(r==NULL)
+       return l;
+       else if(l==NULL)
+       return r;
+       else
+       return root;
+    }
+};
