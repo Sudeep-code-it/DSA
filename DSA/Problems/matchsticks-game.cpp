4373d12f7805ed4137d0336897776fdc5f422375
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2023-06-20 17:52:52 +0530

    matchsticks-game.cpp

---
+++ DSA/Problems/matchsticks-game.cpp
@@ -0,0 +1,9 @@
+// https://practice.geeksforgeeks.org/problems/-matchsticks-game4906/1
+
+class Solution {
+  public:
+    int matchGame(long long N) {
+       if(N%5==0) return -1;
+       else return N%5;
+    }
+};
